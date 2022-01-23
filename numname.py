33962f4a1141a7da517f64ea1f33cf5e03ff0b91
rjbx <bob@coded.art>
2022-01-22 22:08:30 -0500

    Add and update working files

--- numname.py
+++ numname.py
@@ -1,30 +1,41 @@
 # Converts the name of each file in the parameter files address
-# to a number in an ordered sequence starting from the parameter integer.
+# to a number in an ordered sequence starting from the parameter integer;
+# ignores period delimeters up to the parameter index.
 #
 # command line call
 # python
 # import numname
 # instance = numname
-# instance.convert(r"[files address]", 1)
+# instance.convert(r"[files address]", 1, 1)
 
 import os
 
-def convert(address : str, start : int):
+def convert(address : str, start : int, delim : int):
 
     os.chdir(address)
     prev_files = os.listdir(address)
     print(prev_files)
 
-    i = start;
+    n = start;
 
     for prev_name in prev_files:
 
-        new_name = str(i)
+        new_name = str(n)
+
+        i = delim
+        prev_name_arr = prev_name.split(".")
+        arr_len = len(prev_name_arr)
+
+        while i < arr_len:
+
+            new_name = (new_name + "." + prev_name_arr[i])
+            i = (i + 1)
+
+        n = (n + 1)
         os.rename(prev_name, new_name)
-        i = (i + 1)
 
     new_files = os.listdir(address)
     print(new_files)
 
 # internal call (uncomment to run)
-# convert(r"[files address]", 1)
+# convert(r"[files address]", 1, 1)
